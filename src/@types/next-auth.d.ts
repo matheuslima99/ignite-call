7ead4f9078df385e3759d711eebd2df0d3cf832e
mathzsl <maths.soares99@gmail.com>
2023-07-26 18:31:29 -0300

    feat(connect-with-calendar): Creating Prisma Adapter

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth"
+
+declare module "next-auth" {
+
+  interface User {
+    id: string;
+    username: string;
+    fullname: string;
+    email: string;
+    avatar_url: string;
+  }
+}
