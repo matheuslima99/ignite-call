64edf29bb0a447a9b416bae8db1403e33de74be2
mathzsl <maths.soares99@gmail.com>
2023-07-30 15:37:19 -0300

    feat:(calendar-and-availability): calendar visual structure

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
@@ -0,0 +1,10 @@
+import { Box, styled } from "@ignite-ui/react";
+
+export const Container = styled(Box, {
+  maxWidth: "100%",
+  margin: "$6 auto 0",
+  padding: 0,
+  position: "relative",
+
+  display: "grid"
+});
