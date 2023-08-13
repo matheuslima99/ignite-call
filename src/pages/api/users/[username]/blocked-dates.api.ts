de70b884f3dc235668f1f4da7964bfa5b52bf815
mathzsl <maths.soares99@gmail.com>
2023-08-13 14:58:34 -0300

    feat: blocked week days

---
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -0,0 +1,46 @@
+import { prisma } from "@/lib/prisma";
+// import dayjs from "dayjs";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  const username = String(req.query.username);
+  const { year, month } = req.query;
+
+  if (!year || !month) {
+    return res.status(400).json({ message: "Year or month not specified." });
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({ message: "User not found." });
+  }
+
+  const availableWeekDays = await prisma.userTimeInterval.findMany({
+    select: {
+      week_day: true,
+    },
+    where: {
+      user_id: user.id,
+    },
+  });
+
+  const blockedWeekDays = Array.from(Array(7).keys()).filter((weekDay) => {
+    return !availableWeekDays.some(
+      (availableWeekDays) => availableWeekDays.week_day === weekDay
+    );
+  });
+
+  return res.json({ blockedWeekDays });
+}
